5c76d5f77141b58007ffcaee45d20e53785023f0
ilampoornan <ilampooornan@gmail.com>
2025-10-09 05:31:22 +0530

    inference pipeline finished (almost)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, APIRouter
 
 from app.routes.upload import router as upload_router
-# from app.routes.process import router as process_router
+from app.routes.intent import router as intent_router
 # from app.routes.powerbi import router as powerbi_router
 
 app = FastAPI(title="Basket Intent Demo")
 
 app.include_router(upload_router)
-# app.include_router(process_router)
+app.include_router(intent_router)
 # app.include_router(powerbi_router)
